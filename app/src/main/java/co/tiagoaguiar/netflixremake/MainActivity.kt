87266014395789e9d15a18fc2e6ff2bd11c0340e
Thiago-S-Brito <thhiago369@gmail.com>
2024-11-14 19:28:20 -0300

    adição das recyclerviews verticais e horizontais

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -2,10 +2,10 @@ package co.tiagoaguiar.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
 
 
@@ -15,16 +15,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Teste", "onCreate")
 
-        val movies = mutableListOf<Movie>()
-        for(i in 0 until 5) {
-            val movie = Movie(R.drawable.movie_4)
-            movies.add(movie)
+    //lista vertical
+        // categoria 1
+            // Listas horizoltais -> f1 - f2 - f3 - f4 ...
+        // cat 2
+            // Listas horizoltais -> f1 - f2 - f3 - f4 ...
+
+        val categories = mutableListOf<Category>()
+        for (j in 0 until 5) {
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until 15) {
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+
+            val category = Category("cat $j", movies)
+            categories.add(category)
         }
 
-        val adapter = MainAdapter(movies)
+
+        val adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
     }
 
